b13e909066e16d2e6a5e6224fe75fb74cc0a8d67
nesricha <nesrine2000@hotmail.it>
2023-10-05 15:57:43 +0200

    tailwind grid to categorylistcomponent

--- src/components/CategoryListComponent.tsx
+++ src/components/CategoryListComponent.tsx
@@ -11,7 +11,9 @@ export default function CategoryListComponent(props: Prop) {
 
     return <>
 
-    <ul className={`${styles.productsList}`}>
+    {/* <ul className={`${styles.productsList}`}> */}
+
+    <ul className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-4">
 
         {props.products.map(product => <li key={product.id}> <ProductCardComponent product={product}/> </li>)}
 
