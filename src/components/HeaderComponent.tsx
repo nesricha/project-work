66f46a8eb9d10469733783e23fbd427dafdb3f6d
nesricha <nesrine2000@hotmail.it>
2023-10-06 19:58:43 +0200

    header font and spacing

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -2,10 +2,15 @@ import styles from "./styles/Styles.module.css"
 
 export default function HeaderComponent() {
 
-    return <div className="flex flex-col justify-center items-center m-auto px-2 py-3 h-40 md:h-60 max-w-md">
+    return <div className="flex flex-col justify-center items-center m-auto px-2 py-3 h-36 md:h-60 max-w-sm md:max-w-md">
 
-    <h1> CyberShop</h1>
-    <h3>Lorem Ipsum è un testo segnaposto utilizzato nel settore.</h3>
+    <h1 className="font-semibold text-3xl md:text-5xl py-3 md:py-5">
+         <span className="text-4xl md:text-6xl italic font-serif">C</span>yber<span className="text-4xl md:text-6xl italic font-serif">S</span>hop
+         </h1>
+
+    <h3 className="text-lg md:text-2xl text-center">
+        <span className="text-2xl md:text-4xl">L</span>orem Ipsum è un testo segnaposto utilizzato nel settore.
+        </h3>
     
     </div>
 }
