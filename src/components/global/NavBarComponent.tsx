3fc8d253ebf4457c1769f8154459184435e666df
nesricha <nesrine2000@hotmail.it>
2023-10-06 23:09:00 +0200

    nav

--- src/components/global/NavBarComponent.tsx
+++ src/components/global/NavBarComponent.tsx
@@ -11,16 +11,19 @@ export default function NavBarComponent() {
         </a>
 
 {/* big screen */}
-            <ul className=" hidden md:flex grow justify-evenly max-w-sm mx-auto translate-x-1/4">
-            <li><a className="hover:shadow-xl py-2 hover:rounded-md hover:bg-gradient-to-b hover:from-white" href="#" target="_blank">Favorites</a></li>
-            <li><a className="hover:shadow-xl py-2 hover:rounded-md hover:bg-gradient-to-b hover:from-white" href="#" target="_blank">Cart</a></li>
-            <li><a className="hover:shadow-xl py-2 hover:rounded-md hover:bg-gradient-to-b hover:from-white" href="#" target="_blank">Contact Us</a></li>
+            <ul className=" hidden md:flex grow justify-evenly max-w-sm mx-auto lg:translate-x-1/4">
+            <li><a className="text-xl hover:shadow-xl py-2 px-3 hover:rounded-md hover:bg-gradient-to-b hover:from-white" href="#" target="_blank">Favorites</a></li>
+            <li><a className="text-xl hover:shadow-xl py-2 px-3 hover:rounded-md hover:bg-gradient-to-b hover:from-white" href="#" target="_blank">Cart</a></li>
+            <li><a className="text-xl hover:shadow-xl py-2 px-3 hover:rounded-md hover:bg-gradient-to-b hover:from-white" href="#" target="_blank">Contact Us</a></li>
             </ul>
 
             <ul className="hidden md:flex items-center">
-            <li><input type="text" placeholder="Don't search..." className="rounded-md focus-visible:outline-double focus-visible:outline-amber-50 p-1"/></li>
+            <li><input type="text" placeholder="Don't search..." className="rounded-md outline-double outline-yellow-800 focus-visible:outline-amber-200 p-1"/></li>
             <li className="px-2.5"><a href="#" className="py-1 px-6 font-semibold bg-yellow-800 text-amber-50 rounded-md hover:bg-orange-100 hover:text-amber-900">Cerca</a></li>
             </ul>
+
+{/* small screen */}
+
 
 
     </div>
