ea7a82ac588eb1c86d1cfcb09fdb6171c2e7e812
nesricha <nesrine2000@hotmail.it>
2023-10-08 23:33:09 +0200

    product info page

--- src/components/ProductInfoComponent.tsx
+++ src/components/ProductInfoComponent.tsx
@@ -13,47 +13,52 @@ export default function ProductInfoComponent(prop: Prop) {
 
     const [selectedImage, setSelectedImage] = useState<string>(prop.product.images[0])
 
-    return <div className="p-14 flex flex-row flex-wrap ">
+    return <div className="p-10">
 
-        <div className="mx-auto p-3 md:w-6/12">
+        <p>Category: <a href={`/category/${prop.product.category}`} className="underline hover:font-medium "> {prop.product.category.toUpperCase()}</a></p>
 
-           <div className="w-11/12 h-[55vmin] md:h-[30vw] overflow-hidden mx-auto my-4"> 
-            <img src={selectedImage} alt={prop.product.title} className="object-cover mx-auto max-h-full p-1 border border-2 border-light-3 rounded-md" />
-           </div>
+        <div className="p-10 flex flex-row flex-wrap ">
 
-            <div className="flex flex-wrap justify-center">
+            <div className="mx-auto p-3 md:w-6/12">
 
-                {prop.product.images.map(image => <a className={`w-20 m-2 border border-2 border-light-3 p-0.5 rounded-md ${image === selectedImage && " border-4"}`} href="#" onClick={() => setSelectedImage(image)}>
-                    <img src={image} alt={prop.product.title} key={image} className="h-full object-cover" />
-                </a>)}
+                <div className="h-[43vw] md:h-[25vw] vertical-middle overflow-hidden mx-auto my-4">
+                    <img src={selectedImage} alt={prop.product.title} className="object-cover mx-auto max-h-full p-1 border border-2 border-light-3 rounded-md" />
+                </div>
+
+                <div className="flex flex-wrap justify-center">
+
+                    {prop.product.images.map(image => <a className={`w-16 m-2 border border-2 border-light-3 p-0.5 rounded-md ${image === selectedImage && " border-4"}`} href="#" onClick={() => setSelectedImage(image)}>
+                        <img src={image} alt={prop.product.title} key={image} className="h-full object-cover" />
+                    </a>)}
+
+                </div>
 
             </div>
 
+            <div className="min-w-[40vw] p-3 md:w-6/12">
+
+                <h2 className="font-bold text-3xl py-3">{prop.product.title}</h2>
+
+                <h3 className="text-xl pb-5">{prop.product.brand}</h3>
+
+                <span className="font-semibold text-2xl px-1.5 ">{prop.product.price} $  </span>
+
+                <span className="text-lg px-1.5 line-through">{` ${oldPrice} $ `}</span>
+
+               
+                <p className="font-bold text-xl px-1.5 py-3 text-amber-700"> {` - ${prop.product.discountPercentage} % OFF`} </p>
+
+                <p className="font-bold text-xl py-2 pt-8">Description</p>
+                <p>{prop.product.description}</p>
+
+                <p className="font-bold text-xl py-2 pt-8">Rating</p>
+                <p className="text-lg px-1.5">{prop.product.rating} /5</p>
+
+                <p className="font-bold text-xl py-2 pt-8" >Stock: <span className="text-lg px-1.5 font-normal">{prop.product.stock}</span></p>
+            </div>
+
+
         </div>
-
-        <div className="min-w-[40vw] p-3 md:w-6/12">
-
-            <h2 className="font-bold text-3xl py-3">{prop.product.title}</h2>
-
-            <h3 className="font-semibold text-xl py-2">{prop.product.brand}</h3>
-
-            <p>{prop.product.category}</p>
-
-            <p>{prop.product.description}</p>
-
-            <h4>{prop.product.price} $ </h4>
-
-            <p className={`${styles.oldPrice}`}>{` ${oldPrice} $ `}</p>
-
-            <p>{` - ${prop.product.discountPercentage} % OFF`}</p>
-
-            <p>{prop.product.rating} /5</p>
-
-            <p>Stock: {prop.product.stock}</p>
-        </div>
-
-
-
 
     </div>
 }
